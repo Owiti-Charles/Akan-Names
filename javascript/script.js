088fbe8c2df469226c63b64c06b9af8127b9e16e
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-07-13 20:23:44 +0300

    add function to get inputs form form and calculate day

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-var CC, YY, MM, DD, day, dayValue;
+var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
@@ -36,4 +36,13 @@ function validate() {
     return( true );
   }
   
+}
+
+function calculateDayValue(){
+  CC = parseInt(document.getElementById("century").value);
+  YY = parseInt(document.getElementById("year").value);
+  MM = parseInt(document.getElementById("month").value);
+  DD = parseInt(document.getElementById("date").value);
+  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+  return (Math.floor(d));
 }
